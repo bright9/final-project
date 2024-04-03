05356c65082251927fc8a51dd03efca490f3ef1d
bright9 <bright9@gmail.com>
2024-04-03 10:20:06 +0900

    commit

---
+++ order/src/main/java/finalproject/domain/OrderRepository.java
@@ -0,0 +1,10 @@
+package finalproject.domain;
+
+import finalproject.domain.*;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//<<< PoEAA / Repository
+@RepositoryRestResource(collectionResourceRel = "orders", path = "orders")
+public interface OrderRepository
+    extends PagingAndSortingRepository<Order, Long> {}
---
+++ order/src/main/java/finalproject/external/Inventory.java
@@ -0,0 +1,11 @@
+package finalproject.external;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class Inventory {
+
+    private Long id;
+    private Long stock;
+}
